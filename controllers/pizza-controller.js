190c849d71f509a5482fda4c54931f2531c2e6cd
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-02-13 17:26:45 -0500

    adedd routes and controllers

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,55 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // the functions will go in here as methods
+    // get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                // If no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+
+
+    },
+
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({
+                _id: req.params.id
+            }).then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+
+
+    }
+}
+
+module.exports = pizzaController;
